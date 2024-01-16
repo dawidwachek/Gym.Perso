24505ced5a6da299067d25ee36f09c593bcb6b56
dawidwachek <62208885+dawidwachek@users.noreply.github.com>
2024-01-16 17:50:25 +0100

    Update wsgi.py

--- trainingstudio/wsgi.py
+++ trainingstudio/wsgi.py
@@ -6,4 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainingstudio.settings')
 
 application = get_wsgi_application()
 
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 app = application
