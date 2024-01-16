39909832d1b43b27dbd0601cf4a3705085e3c07b
dawidwachek <62208885+dawidwachek@users.noreply.github.com>
2024-01-16 18:09:17 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,16 +2,18 @@
 #python3.10 manage.py collectstatic
 
 echo "Creating a virtual environment..."
-python3.10 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
+
+pip3 install whitenoise
