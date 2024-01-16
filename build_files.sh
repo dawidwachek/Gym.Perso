c1e53e0e4e092417317cc40bbd552c82802869d6
dawidwachek <62208885+dawidwachek@users.noreply.github.com>
2024-01-16 18:31:12 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -16,5 +16,5 @@ python3 -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic
 
