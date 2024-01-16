5bad97ab248e835cc9a80720d0e064f260c21e02
dawidwachek <62208885+dawidwachek@users.noreply.github.com>
2024-01-16 18:03:10 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,17 @@
-pip3.10 install -r requirements.txt
-python3.10 manage.py collectstatic
+#pip3.10 install -r requirements.txt
+#python3.10 manage.py collectstatic
+
+echo "Creating a virtual environment..."
+python3.10 -m venv venv
+source venv/bin/activate
+
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
